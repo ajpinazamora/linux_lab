c6a025e76ef0f9ebba3aaea29a77b1881ed1bc41
ajpinazamora <ajpinazamora@gmail.com>
2025-10-14 11:57:17 -0500

    Formatted output in greet.sh for clarity

--- greet.sh
+++ greet.sh
@@ -2,7 +2,7 @@
 
 if [ $# -gt 0 ]
 then 
-	echo "Welcome to the LINUX Research Lab, $1!"
+	echo "Welcome to the LINUX Research Lab, $*!"
 else
 	read -p "Please enter your name: " NAME
        echo "Welcome to the LINUX Research Lab $NAME!"	
