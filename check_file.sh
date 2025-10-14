e57f4bd5faba715860bf9e0c3841c10a745ea927
ajpinazamora <ajpinazamora@gmail.com>
2025-10-14 12:53:42 -0500

    Added file creation option to check_file.sh

--- check_file.sh
+++ check_file.sh
@@ -4,6 +4,7 @@ echo -e "Enter file name: --> \c"
 read FILENAME
 if [ ! -n "$FILENAME" ]; then
     echo "Error: No file name was received."
+    exit 1
 else
     echo "Looking for $FILENAME..."
 fi
@@ -12,4 +13,14 @@ if [ -f "$FILENAME" ]; then
 	echo "File found"
 else
 	echo "File was not found"
+	echo -e "would you like to creat the file (y/n) --> \c"
+	read ANSWER
+	if [ "$ANSWER" = "y" ] || [ "$ANSWER" = "Y" ]
+	then 
+		touch "$FILENAME"
+		echo "File created: $FILENAME"
+	else
+		echo "Thank you so much, hope to see you back soo"
+		exit 0
+	fi
 fi
