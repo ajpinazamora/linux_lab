37eecd3aac856650de0837be83dca3e4f9e7d5c7
ajpinazamora <ajpinazamora@gmail.com>
2025-10-15 16:03:22 -0500

    Implemented countdown loop in countdown.sh

--- countdown.sh
+++ countdown.sh
@@ -4,8 +4,13 @@ echo -e "Enter a positive number: --> \c"
 read N
 
 if [ -n "$N" ] && [ "$N" -gt 0 ]; then
-	echo "Ready to continue with $N"
+	while [ "$N" -ge 1 ];
+	do
+		echo "$N"
+		N=$((N - 1))
+	done
 else
 	echo "Invalid entry"
 	exit 1
 fi
+
